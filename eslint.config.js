3288c45d678a08808e52dc241f489db20f26aa9c
solidcode79 <49307893+solidcode79@users.noreply.github.com>
2025-12-07 13:21:25 +0530

    Housekeeping: fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,42 @@
-import tsESLint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tsESLint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
+        errorOnTypeScriptSyntacticAndSemanticIssues: true,
       },
     },
     plugins: {
-      "@typescript-eslint": tsESLint,
+      '@typescript-eslint': tsESLint,
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
+      // Typical correctness rules
+      'no-undef': 'error',
+      'no-unreachable': 'error',
+      'no-empty': 'error',
+      'no-unused-expressions': 'error',
+      // TypeScript-level correctness rules
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-redeclare': 'error',
+      '@typescript-eslint/no-dupe-class-members': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+  {
+    files: ['playwright.config.ts'],
+    languageOptions: {
+      globals: {
+        process: 'readonly',
+      },
+      env: {
+        node: true,
+      },
     },
   },
 ];
